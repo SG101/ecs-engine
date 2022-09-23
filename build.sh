60f05f967e2d48e3955863061f8bc3b9c533ebe3
SG101 <scottgerretsen@icloud.com>
2022-09-23 22:47:25 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 GAME_FILES=src/game/*.c
+COMPILER='gcc'
 
-gcc ${GAME_FILES} -c -L"/lib/" -lSDL2 -L"lib/" -lecs -I"include/" -I"src/engine/" -I"/usr/local/include/" -o lib/debuggame.o -DDEBUG
+${COMPILER} ${GAME_FILES} -c -L"/lib/" -lSDL2 -L"lib/" -lecs -I"include/" -I"src/engine/" -I"/usr/local/include/" -o lib/debuggame.o -DDEBUG
 
-gcc ${GAME_FILES} -c -L"/lib/" -lSDL2 -L"lib/" -lecs -I"include/" -I"src/engine/" -I"/usr/local/include/" -o lib/releasegame.o -DNDEBUG
-
-if [ $? -eq 0 ]; then
-	ar rs lib/libreleasegame.a lib/releasegame.o
-	ar rs lib/libdebuggame.a lib/debuggame.o
-
-	if [ $? -eq 0 ]; then
-		ENGINE_FILES=src/engine/*.c
-		rm lib/releasegame.o
-		rm lib/debuggame.o
-		gcc ${ENGINE_FILES} -L"/lib/" -lSDL2 -L"lib/" -ldebuggame -lecs -I "include/" -I"/usr/local/include/" -o bin/debug/engine-deb -DDEBUG
-	
-		if [ $? -eq 0 ]; then
-			gcc ${ENGINE_FILES} -L"/lib/" -lSDL2 -L"lib/" -lreleasegame -lecs -I "include/" -I"/usr/local/include/" -o bin/release/engine-rel -DNDEBUG
-		fi
-	fi
+if [ $? -ne 0 ]; then
+	exit 0
 fi
+
+${COMPILER} ${GAME_FILES} -c -L"/lib/" -lSDL2 -L"lib/" -lecs -I"include/" -I"src/engine/" -I"/usr/local/include/" -o lib/releasegame.o -DNDEBUG
+
+if [ $? -ne 0 ]; then
+	exit 0
+fi
+
+ar rs lib/libreleasegame.a lib/releasegame.o
+ar rs lib/libdebuggame.a lib/debuggame.o
+
+if [ $? -ne 0 ]; then
+	exit 0
+fi
+
+ENGINE_FILES=src/engine/*.c
+rm lib/releasegame.o lib/debuggame.o
+
+${COMPILER} ${ENGINE_FILES} -L"/lib/" -lSDL2 -L"lib/" -ldebuggame -lecs -I "include/" -I"/usr/local/include/" -o bin/debug/engine-deb -DDEBUG
+
+if [ $? -ne 0 ]; then
+	exit 0
+fi
+
+${COMPILER} ${ENGINE_FILES} -L"/lib/" -lSDL2 -L"lib/" -lreleasegame -lecs -I "include/" -I"/usr/local/include/" -o bin/release/engine-rel -DNDEBUG
