b924bc926218fc1e9e1f8dc1ae0d6ec9043464f5
SG101 <scottgerretsen@icloud.com>
2022-09-24 10:42:56 +0200

    added CMake builk files

---
+++ CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/bin/engine"
+  "/bin/engine.pdb"
+  "CMakeFiles/engine.dir/src/engine/debug.c.o"
+  "CMakeFiles/engine.dir/src/engine/debug.c.o.d"
+  "CMakeFiles/engine.dir/src/engine/engine.c.o"
+  "CMakeFiles/engine.dir/src/engine/engine.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
